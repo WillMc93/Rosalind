4de61cee89d89dda06944ecd969d7ba888de9638
Will McElhenney <whm0004@auburn.edu>
2020-02-25 17:13:29 -0600

    polishing when I shouldn't be

--- fasta_tools.py
+++ fasta_tools.py
@@ -2,6 +2,9 @@ from Bio import SeqIO
 import pycurl
 import io
 
+UNIPROT_URL = lambda ID: f'https://www.uniprot.org/uniprot/{ID}.fasta'
+
+# read sequence data from File Object
 def fasta_parse(file_obj):
 	outp = dict()
 
@@ -12,7 +15,8 @@ def fasta_parse(file_obj):
 
 	return outp
 
-def fasta_read(file):
+# read sequence data from file at file_path
+def fasta_read(file_path):
 	outp = dict()
 
 	with open(file) as inp:
@@ -20,11 +24,15 @@ def fasta_read(file):
 
 	return outp
 
+# wrap sequence data in file and read with fasta_parse
+# for use in conjunction with pulling data from the internet
 def fasta_read_raw(fasta):
 	file = io.StringIO(fasta)
-
 	
-	return fasta_read(file)
+	return fasta_parse(file)
 
+# gather fasta data from uniprot
 def get_uniprot_fasta(ID):
-	url = lambda ID: f'https://www.uniprot.org/uniprot/{ID}.fasta'
+	url = UNIPROT_URL(ID)
+
+	data = 
